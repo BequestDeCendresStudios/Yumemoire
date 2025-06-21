5e61b9caa3d160a074d9899760bc5eb236c81685
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-06-20 23:06:33 -0400

    Create title.rb

---
+++ title.rb
@@ -0,0 +1,9 @@
+require_relative "YumemoireFramework/version"
+
+def titlescreen
+  YumemoireFramework::Title.main
+end
+
+#def lunar_calender
+  #YumemoireFramework::LunarCalender.lunar_cycle
+#end
